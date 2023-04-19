125bc3511ea108fbaca1fbf2bd502ca947083949
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2023-04-19 14:20:27 -0700

    Stage up project

--- AttackSwapper/ModCore.cs
+++ AttackSwapper/ModCore.cs
@@ -1,18 +1,22 @@
-﻿using System.Reflection;
+﻿using System.Collections.Generic;
+using System.Reflection;
 using BepInEx;
 using BepInEx.Configuration;
 using HarmonyLib;
 using ServerSync;
+using UnityEngine;
 
-namespace ModFrame
+namespace AttackSwapper
 {
     [BepInPlugin(ModGUID, ModName, ModVersion)]
-    public class NewMod : BaseUnityPlugin
+    public class AttackSwapperMod : BaseUnityPlugin
     {
-        private const string ModName = "New Mod";
-        private const string ModVersion = "1.0";
-        private const string ModGUID = "some.new.guid";
+        private const string ModName = "AttackSwapperMod";
+        private const string ModVersion = "1.0.0";
+        private const string ModGUID = "zarboz.AttackSwapperMod";
         private static Harmony harmony = null!;
+        
+        #region ConfigSync
         ConfigSync configSync = new(ModGUID) 
             { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion};
         internal static ConfigEntry<bool> ServerConfigLocked = null!;
@@ -26,6 +30,13 @@ namespace ModFrame
             return configEntry;
         }
         ConfigEntry<T> config<T>(string group, string name, T value, string description, bool synchronizedSetting = true) => config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+
+        #endregion
+        
+        internal AssetBundle _assetBundle;
+        internal static Dictionary<string, AnimationClip> ExternalAnimations = new();
+        internal static Dictionary<string, string> ReplacementAnimationList = new();
+        internal static bool FirstInit;
         public void Awake()
         {
             Assembly assembly = Assembly.GetExecutingAssembly();
@@ -33,6 +44,13 @@ namespace ModFrame
             harmony.PatchAll(assembly);
             ServerConfigLocked = config("1 - General", "Lock Configuration", true, "If on, the configuration is locked and can be changed by server admins only.");
             configSync.AddLockingConfigEntry(ServerConfigLocked);
+            
+            _assetBundle = Utilities.LoadAssetBundle("attackattack");
+            
+            ExternalAnimations.Add("SkillALL", _assetBundle.LoadAsset<AnimationClip>("Skill_G_ALL"));
+            
+            ReplacementAnimationList.Add("Greatsword BaseAttack (3)", "SkillALL");
+            
         }
     }
 }
---
+++ AttackSwapper/Patches.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using UnityEngine;
+
+namespace AttackSwapper
+{
+    public class Patches
+    {
+        private static RuntimeAnimatorController OriginalPlayerController;
+        private static RuntimeAnimatorController MyNewController;
+
+        [HarmonyPatch(typeof(Player), nameof(Player.Start))]
+        static class Player_Init_Load_Animator
+        {
+           
+            static void Postfix(Player __instance)
+            {
+                if (!AttackSwapperMod.FirstInit)
+                {
+                    AttackSwapperMod.FirstInit = true;
+                    OriginalPlayerController = Utilities.MakeAoc(__instance.m_animator.runtimeAnimatorController, new Dictionary<string,string>(), new());
+                    MyNewController = Utilities.MakeAoc(__instance.m_animator.runtimeAnimatorController, AttackSwapperMod.ReplacementAnimationList, AttackSwapperMod.ExternalAnimations);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(VisEquipment), nameof(VisEquipment.SetRightHandEquiped))]
+        public static class TestEquipPatch
+        {
+            public static void Postfix(VisEquipment __instance)
+            {
+                if (__instance.gameObject.GetComponent<Player>() != null)
+                {
+                    if (__instance.m_rightItemInstance.GetComponent<ItemDrop>().m_itemData.m_shared.m_itemType ==
+                        ItemDrop.ItemData.ItemType.TwoHandedWeapon)
+                    {
+                        Player.m_localPlayer.m_animator.runtimeAnimatorController = MyNewController;
+                    }
+                    else
+                    {
+                        Player.m_localPlayer.m_animator.runtimeAnimatorController = OriginalPlayerController;
+                    }
+                }
+            }
+        }
+    }
+}
